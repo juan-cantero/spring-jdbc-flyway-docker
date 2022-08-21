ce90ac0b745fe0abab52018054d36c24e126a3d1
juan cantero <juan.cantero@outlook.com>
2022-08-21 16:43:48 -0300

    starting config

--- src/main/java/com/amigoscode/DemoApplication.java
+++ src/main/java/com/amigoscode/DemoApplication.java
@@ -1,7 +1,12 @@
 package com.amigoscode;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class DemoApplication {
